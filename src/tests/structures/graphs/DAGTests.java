7732494e9399a8291186879acdc5189810d9959a
Umberto Loria SAM <umberto.loria@gmail.com>
2019-05-15 10:27:35 +0200

    Test di topologie completati.

--- src/tests/structures/graphs/DAGTests.java
+++ src/tests/structures/graphs/DAGTests.java
@@ -6,40 +6,80 @@ import structures.SimpleDGraph;
 public class DAGTests {
 
 	public static void main(String[] args) {
-
-		SimpleDGraph<Integer> g = new SimpleDGraph<>();
-
-		g.add(1);
-		g.add(2);
-		g.add(3);
-		g.add(4);
-		g.add(5);
-		g.add(6);
-		g.add(7);
-
-		g.link(1, 4);
-		g.link(1, 5);
-		g.link(1, 7);
-
-		g.link(2, 3);
-		g.link(2, 5);
-		g.link(2, 6);
-
-		g.link(3, 4);
-		g.link(3, 5);
-
-		g.link(4, 5);
-
-		g.link(5, 6);
-		g.link(5, 7);
-
-		g.link(6, 7);
-
-		List<List<Integer>> topologie = g.topologie();
-		for (List<Integer> topologia : topologie) {
+		SimpleDGraph<Integer> g1 = new SimpleDGraph<>();
+		g1.add(1);
+		g1.add(2);
+		g1.add(3);
+		g1.add(4);
+		g1.add(5);
+		g1.add(6);
+		g1.add(7);
+		g1.link(1, 4);
+		g1.link(1, 5);
+		g1.link(1, 7);
+		g1.link(2, 3);
+		g1.link(2, 5);
+		g1.link(2, 6);
+		g1.link(3, 4);
+		g1.link(3, 5);
+		g1.link(4, 5);
+		g1.link(5, 6);
+		g1.link(5, 7);
+		g1.link(6, 7);
+		System.out.println("primo grafo");
+		for (List<Integer> topologia : g1.topologie()) {
 			System.out.println(topologia);
 		}
-
+		System.out.println();
+		SimpleDGraph<Character> g2 = new SimpleDGraph<>();
+		g2.add('a');
+		g2.add('b');
+		g2.add('c');
+		g2.add('d');
+		g2.add('e');
+		g2.link('a', 'b');
+		g2.link('a', 'c');
+		g2.link('e', 'b');
+		g2.link('c', 'd');
+		g2.link('d', 'e');
+		System.out.println("secondo grafo");
+		for (List<Character> topologia : g2.topologie()) {
+			System.out.println(topologia);
+		}
+		System.out.println();
+		SimpleDGraph<Character> g3 = new SimpleDGraph<>();
+		g3.add('b');
+		g3.add('a');
+		g3.add('c');
+		g3.add('d');
+		g3.add('e');
+		g3.link('a', 'b');
+		g3.link('a', 'c');
+		g3.link('b', 'e');
+		g3.link('c', 'd');
+		g3.link('d', 'e');
+		System.out.println("terzo grafo");
+		for (List<Character> topologia : g3.topologie()) {
+			System.out.println(topologia);
+		}
+		System.out.println();
+		SimpleDGraph<Character> g4 = new SimpleDGraph<>();
+		g4.add('a');
+		g4.add('b');
+		g4.add('c');
+		g4.add('e');
+		g4.add('d');
+		g4.add('f');
+		g4.link('a', 'b');
+		g4.link('b', 'c');
+		g4.link('c', 'f');
+		g4.link('a', 'd');
+		g4.link('d', 'e');
+		g4.link('e', 'f');
+		System.out.println("quarto grafo");
+		for (List<Character> topologia : g4.topologie()) {
+			System.out.println(topologia);
+		}
 	}
 
 }
